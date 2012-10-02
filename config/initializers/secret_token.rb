a4cb4706291b901e95659cb4f21bcf83816ea720
Richard King <rkingucla@ymail.com>
2012-10-01 20:37:24 -0700

    add to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberDemo::Application.config.secret_token = 'd925f8304043caa65bb4d97323f980415df66047bafbb1908b229407495813e14adfd4ae65c20c20d8870bd3f1e345ecc6b306c0221efaea3ee6867ed0a933e4'
